7da527816e731cb7bdcf0d827c7d5d4139d473ec
Isac Zobec <zobec.isac@gmail.com>
2023-10-16 14:12:28 +0200

    Adds comments and some docstrings

--- Labb3.py
+++ Labb3.py
@@ -4,7 +4,7 @@ def aritmetrisksumma(a1, # Första talet i talserien
                     d, # Differensen mellan varje tal
                     n): # Antalet tal i talserien
 
-    return(n*(a1+(a1+d*(n-1)))/2) #returnerar aritmetiska summan
+    return(n*(a1+(a1+d*(n-1)))/2) #returnerar aritmetiska summan enligt dess formel
 
 
 def geometrisksumma(g1, # Första talet i talserien
@@ -12,21 +12,24 @@ def geometrisksumma(g1, # Första talet i talserien
                    n): # Antalet tal i talserien
 
     if q == 1:
-        return g1 * n
+        return g1 * n # blir division med noll enligt formeln om man inte gör såhär
 
-    return(g1*((q**n)-1)/(q-1)) #returnerar den geometriska summan
+    return(g1*((q**n)-1)/(q-1)) # returnerar den geometriska summan enligt dess formel
 
 
 def main():
 
+    # använder felhanteringsmodulen för att se till att alla inputs är av rätt typ
     a1 = felhantering.felhantering_flyttal("Ange ett startvärde (a1): ")
     d = felhantering.felhantering_flyttal("Ange differensen (d): ")
     g1 = felhantering.felhantering_flyttal("Ange startvärde (g1): ")
     q = felhantering.felhantering_flyttal("Ange kvot (q): ")
 
+
+    
     n = felhantering.felhantering_heltal("Ange antalet element (n): ")
     while n < 0:
-        print("n måte vara 0 eller större!")
+        print("n måte vara 0 eller större!") # en talföljd får inte ha ett negativt antal tal!
         n = felhantering.felhantering_heltal("Ange antalet element (n): ")
 
 
--- felhantering.py
+++ felhantering.py
@@ -1,16 +1,18 @@
-def felhantering_flyttal(inmatningMeddelande):
+def felhantering_flyttal(inmatningMeddelande: str) -> float:
+    """Tar input upprepade gånger tills användaren skriver in ett flyttal"""
     while True:
         try:
-            return float(input(inmatningMeddelande))
+            return float(input(inmatningMeddelande)) # returnerar användarens input om det är ett flyttal, annars går koden till except-statementen
         except ValueError:
-            print("Det där va inget flyttal")
+            print("Det där va inget flyttal") # ger ett felmedelande och upprepar loopen 
 
-def felhantering_heltal(inmatningMeddelande):
+def felhantering_heltal(inmatningMeddelande: str) -> int:
+    """Tar input upprepade gånger tills användaren skriver in ett heltal"""
     while True:
         try:
-            return int(input(inmatningMeddelande))
+            return int(input(inmatningMeddelande)) # returnerar användarens input om det är ett heltal, annars går koden till except-statementen
         except ValueError:
-            print("n måste vara ett heltal")
+            print("n måste vara ett heltal") # ger ett felmedelande och upprepar loopen 
 
 
 
